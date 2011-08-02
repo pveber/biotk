2df8ff5b483ecee82bac198e428130908485e0cc
pveber <philippe.veber@gmail.com>
2011-08-03 00:23:47 +0200

    Dependency arguments are expanded too (and some simplifications using quotations)

--- src/syntax/pa_guizmin.ml
+++ src/syntax/pa_guizmin.ml
@@ -1,5 +1,7 @@
 open Camlp4
 
+let ( |> ) x f = f x
+
 module Id : Sig.Id =
 struct
   let name = "pa_guizmin"
@@ -15,32 +17,39 @@ struct
   let param_abstract _loc arg body =
     let arg = match arg with
 	`lab_param (lid,ty) -> 
-	  PaLab (_loc,lid, PaTyc (_loc, 
-				  PaId (_loc, IdLid (_loc, lid)),
-				  TyId (_loc, IdLid (_loc, ty))))
+	  PaLab (_loc,lid, <:patt< ($lid: lid$ : $lid:ty$) >>)
       | `anon_param (lid,ty) -> 
 	  <:patt< ($lid: lid$ : $lid:ty$) >>
       | `opt_param (lid,ty,expr) -> 
-	  PaOlbi (_loc, "", PaId (_loc, IdLid (_loc, lid)),
-		  ExTyc (_loc, expr,
-			 TyId (_loc, IdLid (_loc, ty))))
- 
+	  let e = <:expr< ( $expr$ : $lid:ty$) >> in
+	  PaOlbi (_loc, "", PaId (_loc, IdLid (_loc, lid)), e)
+     in <:expr< fun $arg$ -> $body$ >>
+
+  let dep_abstract _loc arg body =
+    let arg = match arg with 
+      | `anon_dep lid -> <:patt< ($lid: lid$ : _) >>
+      | `anon_list_dep lid -> <:patt< ($lid: lid$ : _ list) >>
     in <:expr< fun $arg$ -> $body$ >>
+    
 
   let abstract _loc ~params ~deps ~body = 
-    List.fold_right (param_abstract _loc) params body
+    List.fold_right (dep_abstract _loc) deps body
+    |> List.fold_right (param_abstract _loc) params 
 
   let expand_process _loc ~params ~deps ~body =
     abstract _loc
       ~params ~deps
       ~body:(<:expr< () >>)
 
+  let list_of_opt = function
+      None -> []
+    | Some l -> l
 
   EXTEND Gram
     GLOBAL: expr;
     expr: LEVEL "top" [
       [ LIDENT "process"; params = LIST0 param ; deps = OPT [ LIDENT "uses" ; deps = LIST1 dep -> deps ] ; "->" ; body = expr ->
-	  expand_process _loc ~params ~deps ~body
+	  expand_process _loc ~params ~deps:(list_of_opt deps) ~body
       ]
     ];
     dep: [
