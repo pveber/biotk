49b8bad747f5ed33211dc71730cdd4e6e01fe220
pveber <philippe.veber@gmail.com>
2012-10-23 15:12:34 +0200

    adjusted signature to use new Bed module

--- src/bioinfo/ucsc.mli
+++ src/bioinfo/ucsc.mli
@@ -6,7 +6,7 @@ val chromosome_sequences : genome -> [`ucsc_chromosome_sequences] dir
 val genome_sequence : genome -> [`fasta] file
 val genome_2bit_sequence : genome -> [`ucsc_2bit] file
 
-val fasta_of_bed : genome -> [`bed] file -> [`fasta] file
+val fasta_of_bed : genome -> 'a Bed.file -> [`fasta] file
 
 
 
