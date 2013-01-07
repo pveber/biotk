30060054959df20a124e45fc78256a36e5338b91
pveber <philippe.veber@gmail.com>
2013-01-07 20:06:53 +0100

    minor fix

--- src/examples/test_mouse_ES.ml
+++ src/examples/test_mouse_ES.ml
@@ -27,7 +27,7 @@ let sample x = Sra.(fastq_dump (sample x))
 let sox2_fastq = List.map sample [ "SRR002023" (* ; "SRR002024" ; "SRR002025" "SRR002025" *) ]
 let gfp_fastq = List.map sample [ "SRR001996" ; "SRR001997" ; "SRR001998" ; "SRR001999" ]
 
-let bowtie_index_mm9 = Bowtie.index (Ucsc.genome_sequence `mm9)
+let bowtie_index_mm9 = Bowtie.index ~packed:true (Ucsc.genome_sequence `mm9)
 
 let sox2_bam = Bowtie.align ~v:2 bowtie_index_mm9 sox2_fastq
 let gfp_bam = Bowtie.align ~v:2 bowtie_index_mm9 gfp_fastq
