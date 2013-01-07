f900c67afde3fb1bda1ca5dd01f92b13fbb3b23f
pveber <philippe.veber@gmail.com>
2013-01-07 14:12:20 +0100

    restaured repo building

--- src/tools/guizmin_repo.ml
+++ src/tools/guizmin_repo.ml
@@ -7,15 +7,23 @@ let item ?(descr = "") path pipeline = Item (pipeline, descr, path)
 
 type t = item list
 
-let create ~base ~repo_base items =
+let create ?(np = 0) ~base ~repo_base items =
   sh "rm -rf %s" repo_base ;
   sh "mkdir -p %s" repo_base ;
   List.iter
     (function Item (pipeline,_,rel_path)  ->
       let abs_path = repo_base ^ "/" ^ (String.concat "/" rel_path) in
+      if np > 0 then Guizmin.build ~base ~np pipeline ;
       sh "mkdir -p %s" (Filename.dirname abs_path) ;
       sh "ln -s `readlink -f %s` %s" (Guizmin.path ~base pipeline) abs_path)
     items
+
+
+
+
+
+
+
 
 
 
--- src/tools/guizmin_repo.mli
+++ src/tools/guizmin_repo.mli
@@ -5,7 +5,9 @@ type item = private Item : 'a Guizmin.pipeline * string * path -> item
 
 val item : ?descr:string -> path -> 'a pipeline -> item
 
-val create : base:string -> repo_base:string -> item list -> unit
+(** if [np] is greater than 0, builds the members of the repo before
+    linking them *)
+val create : ?np:int -> base:string -> repo_base:string -> item list -> unit
 
 
 
