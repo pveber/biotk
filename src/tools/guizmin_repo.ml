6fee5fd928f00b4742e76e34a84d9ca54de8f871
pveber <philippe.veber@gmail.com>
2013-02-01 08:48:15 +0100

    tools/Repo: checks unicity of paths in repo upon creation

--- src/tools/guizmin_repo.ml
+++ src/tools/guizmin_repo.ml
@@ -1,3 +1,5 @@
+open Core.Std
+
 open GzmUtils
 
 type path = string list
@@ -7,19 +9,32 @@ let item ?(descr = "") path pipeline = Item (pipeline, descr, path)
 
 type t = item list
 
+let find_duplicate_paths repo =
+  List.(
+    map repo ~f:(function Item (_,_,x) -> x)
+    |! find_a_dup
+  )
+
+let fail_on_duplicate_paths items = match find_duplicate_paths items with
+| None -> ()
+| Some p ->
+    let path = String.concat ~sep:"/" p in
+    failwithf "Path %s is present several times in the repo!" path ()
+
 let create ?(np = 0) ~base ~repo_base items =
+  fail_on_duplicate_paths items ;
   sh "rm -rf %s" repo_base ;
   sh "mkdir -p %s" repo_base ;
-  List.iter
-    (function Item (pipeline,_,rel_path)  ->
-      let abs_path = repo_base ^ "/" ^ (String.concat "/" rel_path) in
+  List.iter items ~f:(
+    function Item (pipeline,_,rel_path)  ->
+      let abs_path = repo_base ^ "/" ^ (String.concat ~sep:"/" rel_path) in
       if np > 0 then Guizmin.build ~base ~np pipeline ;
       bash [
         sp "mkdir -p %s" (Filename.dirname abs_path) ;
         sp "ln -s `readlink -f %s` %s" (Guizmin.path ~base pipeline) abs_path
-      ])
-    items
-
+      ]
+  )
+    
 
 
 
