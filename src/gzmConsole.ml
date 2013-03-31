b26013e65022a70ff1667504394a8ebee6dd0348
pveber <philippe.veber@gmail.com>
2013-03-31 16:16:13 +0200

    console: api to delete files in the cache

--- src/gzmConsole.ml
+++ src/gzmConsole.ml
@@ -12,59 +12,23 @@ let rec du path =
       Array.fold sizes ~init:stats.st_size ~f:Int64.( + )
   | _ -> 0L
 
-let cache_strata (base_directory : base_directory) =
-  let cache_dir = Filename.concat (base_directory :> string) "cache" in
-  let files = Sys.readdir cache_dir in
-  let files_w_stats = Array.map files ~f:(
-    fun f -> 
-      let path = Filename.concat cache_dir f in
-      Unix.((stat path).st_mtime), du path, f
-  )
-  in
-  Array.sort compare files_w_stats ;
-  Array.fold files_w_stats ~init:([], 0L) ~f:(
-    fun (l, sum) (mtime, size, f) ->
-      let sum' = Int64.(sum + size) in
-      (f, Unix.localtime mtime, size, sum') :: l, sum'
-  )
-  |! fst 
-  |! List.rev
-
-let cache_stratum (base_directory : base_directory) space =
-  let space = Int64.(1024L * 1024L * of_int space) in
-  let strata = cache_strata base_directory in
-  List.find_map strata ~f:(fun (file, tm, _, sum) -> if sum > space then Some (file, tm, sum) else None)
-
-let cache_stratum_hum (base_directory : base_directory) space =
-  match cache_stratum base_directory space with
-  | Some (file, tm, space) -> Some (
-    file, 
-    (Unix.strftime tm "%F"),
-    (float Int64.(to_int_exn (space / (1024L * 1024L))) /. 1024.)
-  )
-  | None -> None
-      
-let clear_cache (base_directory : base_directory) space =
-  let cache_dir = Filename.concat (base_directory :> string) "cache" in
-  let space = Int64.(1024L * 1024L * of_int space) in
-  let strata = cache_strata base_directory in
-  List.filter_map strata ~f:(fun (file, tm, _, sum) -> 
-    if sum <= space then Some (Filename.concat cache_dir file) 
-    else None
-  )
-  |! List.iter ~f:(fun f -> GzmUtils.sh "rm -rf %s" f)
-
 type history = ([`used | `requested | `built] * CalendarLib.Printer.Date.t) list
 
 let tag_of_string = function
 | "USED" -> `used
 | "REQ" -> `requested
 | "BUILT" -> `built
-| _ -> assert false
+| l -> invalid_argf "gzmConsole.tag_of_string: %s" l ()
 
 let parse_history_line l =
-  let tag, stamp = String.lsplit2_exn l ~on:':' in
-  tag_of_string tag, CalendarLib.Printer.Date.from_string (String.lstrip stamp)
+  let stamp, tag = String.lsplit2_exn l ~on:':' in
+  try
+    tag_of_string (String.lstrip tag), CalendarLib.Printer.Date.from_string stamp
+  with 
+  | Invalid_argument _ as e -> raise e
+  | e -> raise e (*
+    invalid_argf "gzmConsole.parse_history_line: %s" l ()
+  *)
 
 let load_history path =
   if Caml.Sys.file_exists path then
@@ -77,7 +41,7 @@ type cached_file = {
   history : history ;
   size : Int64.t
 }
-type cache_selection = cached_file list
+type cache_selection = base_directory * cached_file list
 
 let less_tagged tag ~during:period ~than:limit x =
   let open CalendarLib in
@@ -96,7 +60,12 @@ let cache_selection
   and req_less_than  = 
     Option.value_map req_less_than ~default ~f:(fun (ntimes, period) -> 
       less_tagged `requested ~than:ntimes ~during:period
-    ) 
+    )
+  and bigger_than =
+    Option.value_map bigger_than ~default ~f:(fun size_limit ->
+      let size_limit = Int64.of_float (1024. *. 1024. *. 1024. *. size_limit) in
+      fun x -> x.size >= size_limit
+    )
   in
   let files = Sys.readdir (cache_dir base)  in
   let cached_files = Array.map files ~f:(
@@ -108,8 +77,21 @@ let cache_selection
       }
   )
   in
-  Array.filter cached_files ~f:(fun x -> used_less_than x && req_less_than x) 
-  |! Array.to_list
+  let files = 
+    Array.filter cached_files ~f:(fun x -> used_less_than x && req_less_than x && bigger_than x) 
+    |! Array.to_list
+  in
+  (base, files)
+
+let size_of_selection (_,l) =
+  List.fold_left l ~init:0L ~f:(fun accu x -> Int64.(accu + x.size))
+  |! (fun sum -> Int64.(sum / (1024L * 1024L) |! to_float) /. 1024.)
+
+
+let clear_selection (base, l) =
+  List.iter l ~f:(fun f -> 
+    GzmUtils.sh "rm -rf %s" (Filename.concat (cache_dir base) f.name)
+  )
 
 
 
--- src/gzmConsole.mli
+++ src/gzmConsole.mli
@@ -1,16 +1,16 @@
 open GzmCore
 
-val cache_strata : base_directory -> (string * Unix.tm * Int64.t * Int64.t) list
-val cache_stratum : base_directory -> int -> (string * Unix.tm * Int64.t) option
-val cache_stratum_hum : base_directory -> int -> (string * string * float) option
-val clear_cache : base_directory -> int -> unit
-
 type cache_selection
 val cache_selection : 
   ?used_less_than:(int * int) -> 
   ?req_less_than:(int * int) -> 
-  ?bigger_than:int ->
+  ?bigger_than:float -> (** size in GB *)
   base_directory -> cache_selection
+
+(** human-readable size in GB *)
+val size_of_selection : cache_selection -> float
+
+val clear_selection : cache_selection -> unit
 
 
 
