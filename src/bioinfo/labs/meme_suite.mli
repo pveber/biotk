e004ddeabe7bb052d5a84b47e28b222f0f9cc099
pveber <philippe.veber@gmail.com>
2013-07-25 17:57:08 +0200

    labs: module to use MemeSuite tools

---
+++ src/bioinfo/labs/meme_suite.mli
@@ -0,0 +1,11 @@
+open Guizmin
+
+type meme_chip_output
+val meme_chip : 
+  ?meme_nmotifs:int -> 
+  ?meme_minw:int ->
+  ?meme_maxw:int ->
+  Fasta.file -> meme_chip_output dir
+val meme_chip_meme_motifs : meme_chip_output dir_path -> (string * Biocaml_pwm.count_matrix) list
+
+val meme2images : ?rc:unit -> [`meme_motif] file -> [`meme2image_output] dir
