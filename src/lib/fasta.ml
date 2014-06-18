40d328299d35afd9e6fa1ad03793262131df871e
pveber <philippe.veber@gmail.com>
2014-06-18 23:22:55 +0200

    fasta: parser implementation (untested)

--- src/lib/fasta.ml
+++ src/lib/fasta.ml
@@ -1,45 +1,87 @@
 open Core_kernel.Std
 open CFStream
 
-type item = {
-  comment : string ;
-  seq : string
+type 'a item = {
+  header : string ;
+  sequence : 'a
 }
+
+type parsing_error = string * int
 
 let chopl s = String.(sub s ~pos:1 ~len:(length s - 1))
 
-let is_comment x = String.is_prefix x ~prefix:">"
+type line =
+  | Header of string
+  | Sequence of string
+  | Empty
 
-let read_sequence lines =
-  let open Or_error in
-  match Stream.next lines with
-  | None -> error_string "Fasta.read: incorrect syntax"
-  | Some l ->
-    let rec loop accu =
-      match Stream.next lines with
-      | None -> accu, lines
-      | Some l ->
-        if is_comment l then accu, Stream.append (Stream.singleton l) lines
-        else loop (accu ^ l)
-    in
-    if is_comment l then error_string "Fasta.read: incorrect syntax"
-    else return (loop l)
+type 'a op = < of_string : string -> ('a, string) Result.t ;
+               to_string : 'a -> string ;
+               concat : 'a list -> 'a >
 
-let read_item lines =
-  let open Or_error in
-  match Stream.next lines with
-  | None -> None
-  | Some l ->
-    if String.is_prefix l ~prefix:">" then
-      let comment = chopl l in
-      match read_sequence lines with
-      | Result.Ok (seq, lines) ->
-        Some (return { comment ; seq }, lines)
-      | Result.Error e -> Some (Result.Error e, Stream.empty ())
-    else
-      Some (error_string "Fasta.read_item: incorrect syntax", Stream.empty ())
+let classify_line = function
+  | "" -> Empty
+  | s when s.[0] = '>' -> Header (chopl s)
+  | s -> Sequence s
 
-let of_channel ic =
-  Biocaml_lines.of_channel ic
-  |> Stream.map ~f:(fun x -> (x : Biocaml_line.t :> string))
-  |> Stream.unfold ~f:read_item
+let read_line ic =
+  Option.map (In_channel.input_line ic) ~f:classify_line
+
+let read_sequence op loc ic =
+  let open Result in
+  let rec loop loc accu = match read_line ic with
+    | None -> return (accu, None)
+    | Some (Sequence s) -> (
+        match op#of_string s with
+        | Ok x -> loop (loc + 1) (x :: accu)
+        | Error e -> fail (e, loc)
+      )
+    | Some l -> return (accu, Some (loc, l))
+  in
+  loop loc [] >>= function
+  | [], _ ->
+    fail ("Missing sequence after header", loc)
+  | (seqs, cursor) ->
+    return (op#concat seqs, cursor)
+
+
+let read_item op (loc, l) ic =
+  let open Result in
+  match l with
+  | Header header -> (
+    match read_sequence op (loc + 1) ic with
+    | Ok (sequence, cursor) ->
+      return { header ; sequence }, cursor
+    | Error e -> fail e, None
+    )
+  | _ -> fail ("expected_header", loc), None
+
+let read_gen op ic =
+  let f cursor = Option.map cursor ~f:(fun l -> read_item op l ic) in
+  let init = Option.map (read_line ic) ~f:(fun l -> 1, l) in
+  Stream.unfold init ~f
+
+let string_ops =
+  object
+    method of_string x = Result.Ok x
+    method to_string x = x
+    method concat x = String.concat x
+  end
+
+let read = read_gen string_ops
+
+let ints_ops =
+  object
+    method of_string s =
+      try
+        String.split ~on:' ' s
+        |> List.map ~f:Int.of_string
+        |> Result.return
+      with Failure _ -> Result.fail "Failed to parse ints"
+    method to_string is =
+      List.map is ~f:Int.to_string
+      |> String.concat ~sep:" "
+    method concat = List.concat
+  end
+
+let read_ints = read_gen ints_ops
--- src/lib/fasta.mli
+++ src/lib/fasta.mli
@@ -1,9 +1,13 @@
 open Core_kernel.Std
 
-type item = {
-  comment : string ;
-  seq : string
+type 'a item = {
+  header : string ;
+  sequence : 'a
 }
 
-val of_channel : in_channel  -> item Or_error.t Stream.t
+type parsing_error = string * int
+
+val read : in_channel -> (string item,  parsing_error) Result.t Stream.t
+val read_ints : in_channel -> (int list item, parsing_error) Result.t Stream.t
+
 (* val to_channel : out_channel -> item Stream.t -> unit *)
