21a730756a4e82d41aadbb4bbbdd12f2874544b1
Philippe Veber <philippe.veber@gmail.com>
2019-01-06 16:47:48 +0100

    new Pwm_stats module

---
+++ lib/pwm_stats.mli
@@ -0,0 +1,29 @@
+(** Implementation of {https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2238751/}*)
+
+val random_sequence : int -> Pwm.background -> string
+
+module TFM_pvalue : sig
+  val score_distribution :
+    ?alpha:float ->
+    ?beta:float ->
+    Pwm.t ->
+    Pwm.background ->
+    (float * float) list
+
+  val fast_pvalue :
+    Pwm.t ->
+    Pwm.background ->
+    float ->
+    float
+
+  val score_of_pvalue :
+    Pwm.t ->
+    Pwm.background ->
+    float ->
+    float
+end
+
+val naive_score_distribution :
+  Pwm.t ->
+  Pwm.background ->
+  (float * float) list
