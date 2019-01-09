fecba647d106875d4d12727792eac11abc14b45d
Philippe Veber <philippe.veber@gmail.com>
2019-01-10 00:02:15 +0100

    Bam_iterator: linear index optimisation

--- lib/bam_iterator.ml
+++ lib/bam_iterator.ml
@@ -2,7 +2,7 @@ open Core_kernel
 open Rresult
 open Biocaml_unix
 
-type index = Bai.bin Int.Table.t String.Table.t
+type index = (Bai.reference_sequence * Bai.bin Int.Table.t) String.Table.t
 
 type t = {
   bam : Bgzf.in_channel ;
@@ -15,7 +15,7 @@ let make_index bai bam_header : index =
     let r = Int.Table.create () in
     let bins = bai_reference_sequence.Bai.bins in
     Array.iter bins ~f:(fun b -> Int.Table.set r ~key:b.Bai.bin ~data:b) ;
-    (name, r)
+    (name, (bai_reference_sequence, r))
   in
   let reference_sequences =
     (Bam.Header.to_sam bam_header).ref_seqs
@@ -54,31 +54,49 @@ let process_chunk iterator f ~(loc : GLoc.t) acc chunk =
       | None -> raise (Interrupt "Expected more alignments, truncated file?")
       | Some (Error e) -> raise (Interrupt (Core.Error.to_string_hum e))
       | Some (Ok al) ->
-        match Bam.Alignment0.(pos al, tlen al) with
-        | (Some pos, Some tlen) ->
+        match Bam.Alignment0.(pos al, seq al) with
+        | (Some pos, Some seq) ->
           let pos = pos - 1 in (* pos is 1-based *)
-          if pos + Int.abs tlen < loc.lo then loop acc
+          let len = String.length seq in
+          if pos + len < loc.lo then loop acc
           else if pos > loc.hi then acc
           else (
-            (* printf "%d %d\n" pos (pos + Int.abs tlen) ; *)
+            (* printf "%d %d\n" pos (pos + len) ; *)
             loop (f acc iterator.bam_header al)
           )
         | _ -> loop (f acc iterator.bam_header al)
   in
-  Bgzf.seek_in iterator.bam chunk.Bai.chunk_beg ;
+  Bgzf.seek_in iterator.bam chunk.chunk_beg ;
   loop acc
+
+let linear_bound idx start =
+  let i = start lsr 14 in
+  if i < idx.Bai.n_intv then
+    let Ioffset o = idx.Bai.intervals.(i) in
+    Some o
+  else None
+
+let consider_chunk linear_bound (chunk : Bai.chunk) =
+  match linear_bound with
+  | None -> true
+  | Some l -> l <= chunk.chunk_end
 
 let fold0 ~bam ~bai ~(loc : GLoc.t) ~init ~f =
   let open Result.Let_syntax in
   let%bind iterator = create ~bam ~bai in
   match String.Table.find iterator.index loc.chr with
   | None -> R.error_msgf "Unknown reference sequence %s" loc.chr
-  | Some idx ->
+  | Some (refseq, idx) ->
+    let linear_bound = linear_bound refseq loc.lo in
     try R.ok @@
       Bai.reg2bins loc.lo loc.hi ~init ~f:(fun acc i ->
         match Int.Table.find idx i with
         | None -> acc
         | Some bin ->
-          Array.fold bin.chunks ~init:acc ~f:(process_chunk iterator ~loc f)
+          Array.fold bin.chunks ~init:acc ~f:(fun acc chunk ->
+              if consider_chunk linear_bound chunk then
+                process_chunk iterator ~loc f acc chunk
+              else acc
+            )
         )
     with Interrupt msg -> Error (`Msg msg)
