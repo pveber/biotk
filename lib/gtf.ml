d2ac7d4524ba6c1e90524e6e9f387512e63882a2
Philippe Veber <philippe.veber@gmail.com>
2019-04-11 18:14:27 +0200

    Gtf: added Annotation module

--- lib/gtf.ml
+++ lib/gtf.ml
@@ -18,3 +18,68 @@ module Item = struct
 end
 
 include Line_oriented.Make(Item)
+
+module Record = struct
+  type t = Gff.record
+  let loc r = GLoc.{ chr = r.Gff.seqname ; lo = r.start_pos ; hi = r.stop_pos }
+end
+
+module Annotation = struct
+  type t = Gff.record list String.Table.t
+
+  let of_items ~id_label items =
+    Stream.of_list items
+    |> CFStream.Stream.filter_map ~f:(function
+        | `Comment _ -> None
+        | `Record r ->
+          match List.Assoc.find r.Biocaml_base.Gff.attributes id_label ~equal:String.equal with
+          | Some (id :: _) -> Some (id, r)
+          | Some []
+          | None -> None
+      )
+    |> Biocaml_unix.Accu.relation
+    |> CFStream.Stream.to_list
+    |> String.Table.of_alist_exn
+
+  let strand_of_entry items =
+    let strands =
+      List.map items ~f:(fun i -> i.Gff.strand)
+      |> List.dedup_and_sort ~compare:Poly.compare
+    in
+    match strands with
+    | [] -> raise (Invalid_argument "strand_of_entry")
+    | [ s ] -> Ok s
+    | _ -> Or_error.error_string "more than one strand in entry"
+
+  let gene_of_entry items =
+    let exons =
+      List.filter_map items ~f:(fun r ->
+          match r.Gff.feature with
+          | Some "exon" -> Some r
+          | _ -> None
+        )
+    in
+    match exons with
+    | [] -> Ok None
+    | _ ->
+      let open Or_error.Monad_infix in
+      strand_of_entry exons >>= function
+      | `Unknown | `Not_stranded -> Or_error.error_string "no defined strand"
+      | `Plus | `Minus as strand ->
+        Gene.make ~strand ~exons:(List.map exons ~f:Record.loc)
+        |> Or_error.map ~f:Option.some
+
+  let genes annot =
+    let exception E of Error.t in
+    try
+      Ok (
+        String.Table.to_alist annot
+        |> List.filter_map ~f:(fun (id, items) ->
+            match gene_of_entry items with
+            | Ok (Some g) -> Some (id, g)
+            | Ok None -> None
+            | Error e -> raise (E e)
+          )
+      )
+    with E msg -> Error msg
+end
--- lib/gtf.mli
+++ lib/gtf.mli
@@ -1,3 +1,4 @@
+open Core_kernel
 open Biocaml_base
 
 module Item : sig
@@ -6,4 +7,20 @@ module Item : sig
   val to_record : t -> Gff.record option
 end
 
+module Record : sig
+  type t = Gff.record
+  val loc : t -> GLoc.t
+end
+
 include Line_oriented.S with type item = Item.t
+
+module Annotation : sig
+  type t
+
+  val of_items :
+    id_label:string ->
+    Item.t list ->
+    t
+
+  val genes : t -> (string * Gene.t) list Or_error.t
+end
