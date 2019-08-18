cc7d5aa556069838fe3508c227f21f0b67b3c4cf
Philippe Veber <philippe.veber@gmail.com>
2019-08-18 22:22:49 +0200

    Profile_matrix: y axis

--- lib/profile_matrix.ml
+++ lib/profile_matrix.ml
@@ -44,10 +44,25 @@ module Make(A : Alphabet) = struct
     Array.map weights ~f:(fun w -> w /. total)
 
   let xlnx x =
-    if x = 0. then 0. else x *. Float.log x
+    if x = 0. then 0. else x *. Float.log x /. Float.log 2.
 
   let column_entropy p =
     sum (Array.length p) ~f:(fun j -> -. xlnx p.(j))
+
+  let draw_y_scale () =
+    let open Croquis.Picture in
+    let text x y msg =
+      text ~x ~y msg ~font:Croquis.Font.free_sans_bold ~size:0.1
+    in
+    blend [
+      path [ 0., 0. ; 0., 2. ] ;
+      path [ -. 0.1, 0. ; 0., 0. ] ;
+      path [ -. 0.1, 1. ; 0., 1. ] ;
+      path [ -. 0.1, 2. ; 0., 2. ] ;
+      text (-. 0.2) 0. "0" ;
+      text (-. 0.2) 1. "1" ;
+      text (-. 0.2) 2. "2" ;
+    ]
 
   let draw t =
     let open Croquis in
@@ -55,7 +70,6 @@ module Make(A : Alphabet) = struct
     let open Gg in
     let size = 10. in
     let font = Font.free_sans_bold in
-    let delta = 0.86 *. size *. (Font.ascender font) in
     let letter =
       List.map A.all ~f:(fun c ->
           sprintf "%c" (Char.uppercase (A.to_char c))
@@ -68,17 +82,19 @@ module Make(A : Alphabet) = struct
       else
         Array.init A.card ~f:(fun i -> Color.gray (float i *. 127. /. float A.card))
     in
-    let left = 3.5 and right = 3.4 in
+    let left = 3.5 and right = 3.5 in
     let draw_letter ~x ~y ~col ~sy l =
       if sy < 1e-6 then None
       else
         let xmin = x -. left in
-        let ymin = y -. 0.075 *. delta in
+        let ymin = y -. Font.(ymax font +. ymin font) *. size /. 2. *. 1.1 in
         let xmax = x +. right in
-        let ymax = y +. 0.925 *. delta in
+        let ymax = 0.425 *. size *. Font.ymax font in
         let bbox = Box2.of_pts (V2.v xmin ymin) (V2.v xmax ymax) in
         Some (
-          crop (text ~size ~font l ~col ~x ~y) bbox
+          (* blend2 *)
+            (crop (text ~size ~font l ~col ~x ~y) bbox)
+            (* (rect ~draw:Color.black ~xmin ~xmax ~ymin ~ymax ()) *)
           |> scale ~sy
         )
     in
@@ -89,13 +105,18 @@ module Make(A : Alphabet) = struct
       |> List.filter_opt
       |> vstack ~align:`centered
     in
-    Array.map t ~f:draw_col
-    |> Array.to_list
-    |> List.mapi ~f:(fun i pic ->
-        let ent = column_entropy t.(i) in
-        scale ~sy:(2. -. ent) pic
-      )
-    |> hstack ~align:`bottom
+    let entropy = Array.map ~f:column_entropy t in
+    let max_entropy = Owl.Stats.max entropy in
+    let logo =
+      Array.map t ~f:draw_col
+      |> Array.to_list
+      |> List.mapi ~f:(fun i pic ->
+          scale ~sy:(2. -. entropy.(i)) pic
+        )
+      |> hstack ~align:`bottom
+      |> reshape ~bbox:(Box2.(v V2.zero V2.(v (float (Array.length t)) max_entropy)))
+    in
+    blend2 (draw_y_scale ()) logo
 end
 
 module DNA = Make(Nucleotide)
