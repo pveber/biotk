e617ec9d24c489b96364d2d06eeba61a1c3aefdd
Philippe Veber <philippe.veber@gmail.com>
2019-08-19 16:54:53 +0200

    Misc: added prod + refactoring

--- lib/misc.ml
+++ lib/misc.ml
@@ -1,8 +1,14 @@
 open Core_kernel
 
-let sum n ~f =
+let int_fold a b ~init ~f =
   let rec loop acc i =
-    if i = n then acc
-    else loop (acc +. f i) (i + 1)
+    if i = b then acc
+    else loop (f acc i) (i + 1)
   in
-  loop 0. 0
+  loop init a
+
+let sum n ~f =
+  int_fold 0 n ~init:0. ~f:(fun acc i -> acc +. f i)
+
+let prod n ~f =
+  int_fold 0 n ~init:1. ~f:(fun acc i -> acc *. f i)
