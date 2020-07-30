2edda7152b0238d27119a66429113972a9d1a3e3
Philippe Veber <philippe.veber@gmail.com>
2020-07-30 09:25:54 +0200

    Nucleotide: added of_int, of_int_exn

--- lib/nucleotide.ml
+++ lib/nucleotide.ml
@@ -8,6 +8,7 @@ let g = 'g'
 let t = 't'
 
 let all = [a;c;g;t]
+let all_array = Array.of_list all
 
 let to_char x = x
 
@@ -22,6 +23,15 @@ let of_char_exn c =
   match of_char c with
   | Some c -> c
   | None -> invalid_arg "Invalid nucleotide"
+
+let of_int i =
+  if i < 0 || i >= card then None
+  else Some all_array.(i)
+
+let of_int_exn n =
+  match of_int n with
+  | None -> raise (Invalid_argument "of_int_exn")
+  | Some n -> n
 
 let to_int = function
   | 'a' -> 0
--- lib/nucleotide.mli
+++ lib/nucleotide.mli
@@ -9,5 +9,7 @@ val card : int
 val to_char : t -> char
 val of_char : char -> t option
 val of_char_exn : char -> t
+val of_int : int -> t option
+val of_int_exn : int -> t
 val to_int : t -> int
 val complement : t -> t
