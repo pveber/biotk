1f94c72a7bc95038a98e72a2901b7b84fe15941e
Philippe Veber <philippe.veber@gmail.com>
2023-01-21 17:54:51 +0100

    Fastq: stop using biocaml

--- lib/fastq.ml
+++ lib/fastq.ml
@@ -1,21 +1,50 @@
 open Core
-open Biocaml_unix
 
-type item = Fastq.item
+type item = {
+  description : string ;
+  sequence : string ;
+  comment : string ;
+  quality : string ;
+}
+[@@deriving sexp]
 
-exception Abort of Error.t
+type parsing_error = [
+  | `Malformed_line of int * string
+  | `Unexpected_EOF
+]
+[@@deriving sexp]
+
+let or_eof = function
+  | None -> Error `Unexpected_EOF
+  | Some x -> Ok x
+
+let read_annotation prefix line_number ic =
+  let open Let_syntax.Result in
+  let* c = In_channel.input_char ic |> or_eof in
+  if Char.equal c prefix then
+    In_channel.input_line ic |> or_eof
+  else Error (`Malformed_line (line_number, sprintf "Expected character '%c' at beginning of line" prefix))
+
+let read_sequence ic =
+  In_channel.input_line ic |> or_eof
+
+let read_item line_number ic =
+  let open Let_syntax.Result in
+  let* description = read_annotation '@' line_number ic in
+  let* sequence = read_sequence ic in
+  let* comment = read_annotation '+' (line_number + 2) ic in
+  let+ quality = read_sequence ic in
+  { description ; sequence ; comment ; quality }
 
 let fold_file fn ~init ~f =
   In_channel.with_file fn ~f:(fun ic ->
-      try
-        Fastq.read ic
-        |> CFStream.Stream.fold ~init ~f:(fun acc item_or_error ->
-            match item_or_error with
-            | Ok it -> f acc it
-            | Error e -> raise (Abort e)
-          )
-        |> Or_error.return
-      with Abort e -> Error e
+      let rec loop ~lno acc =
+        let open Let_syntax.Result in
+        let* item = read_item lno ic in
+        let* acc' = f acc item in
+        loop ~lno:(lno + 4) acc'
+      in
+      loop ~lno:0 init
     )
 
 module Stats = struct
@@ -23,7 +52,7 @@ module Stats = struct
     nb_reads : int ;
   }
   let of_file fq =
-    let open Or_error.Let_syntax in
-    let%map nb_reads = fold_file fq ~init:0 ~f:(fun n _ -> n + 1) in
+    let open Let_syntax.Result in
+    let+ nb_reads = fold_file fq ~init:0 ~f:(fun n _ -> Ok (n + 1)) in
     { nb_reads }
 end
--- lib/fastq.mli
+++ lib/fastq.mli
@@ -1,17 +1,28 @@
 open Core
-open Biocaml_unix
 
-type item = Fastq.item
+type item = {
+  description : string ;
+  sequence : string ;
+  comment : string ;
+  quality : string ;
+}
+[@@deriving sexp]
+
+type parsing_error = [
+  | `Malformed_line of int * string
+  | `Unexpected_EOF
+]
+[@@deriving sexp]
 
 val fold_file :
   string ->
   init:'a ->
-  f:('a -> item -> 'a) ->
-  'a Or_error.t
+  f:('a -> item -> ('a, 'b) result) ->
+  ('a, [> parsing_error ] as 'b) result
 
 module Stats : sig
   type t = {
     nb_reads : int ;
   }
-  val of_file : string -> t Or_error.t  
+  val of_file : string -> (t, [> parsing_error]) result
 end
