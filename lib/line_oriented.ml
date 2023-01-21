2b8b4f77b761df0f1ce5f9beb1c3ab5cbb6f959d
Philippe Veber <philippe.veber@gmail.com>
2023-01-21 17:51:02 +0100

    Line_oriented: added Parser and fold functions

--- lib/line_oriented.ml
+++ lib/line_oriented.ml
@@ -1,5 +1,57 @@
 open Core
 
+
+module Parser = struct
+  type state =
+    | Current_line of
+        { n : int
+        ; value : Line.t
+        }
+    | Finished of { n : int }
+  (* [n] represents the number of completed lines *)
+
+  let initial_state = Current_line { n = 0; value = Line.empty }
+
+  let line_number = function
+    | Current_line { n; value } -> if String.equal (value :> string) "" then n else n + 1
+    | Finished { n } -> n
+
+  let step st i =
+    match st, i with
+    | Finished _, _ -> st, []
+    | Current_line { n; value }, None ->
+      Finished { n }, if Line.is_empty value then [] else [ value ]
+    | Current_line { n; value }, Some input -> (
+      match Line.rightmost input with
+      | None, line ->
+        let value = Line.append value line in
+        Current_line { n; value }, []
+      | Some left, right -> (
+        match Line.parse_string left with
+        | [] -> assert false (* [Line.parse_string] never returns an empty list *)
+        | h :: t ->
+          ( Current_line { n = n + List.length t + 1; value = right }
+          , Line.append value h :: t )))
+end
+
+let fold_err fn ~init ~f =
+  In_channel.with_file fn ~f:(fun ic ->
+      let rec loop lno acc =
+        match In_channel.input_line ic with
+        | None -> Ok acc
+        | Some l -> (
+            match f acc lno (Line.of_string_unsafe l) with
+            | Ok acc' -> loop (succ lno) acc'
+            | Error e -> Error e
+          )
+      in
+      loop 0 init
+    )
+
+let fold fn ~init ~f =
+  fold_err fn ~init ~f:(fun acc lno x -> Ok (f acc lno x))
+  |> Rresult.R.get_ok
+
 module type Item = sig
   type t
   val parse : Line.t -> t
@@ -24,13 +76,8 @@ module Make(Item : Item) = struct
       )
 
   let fold fn ~init ~f =
-    In_channel.with_file fn ~f:(fun ic ->
-        let rec loop acc =
-          match In_channel.input_line ic with
-          | None -> acc
-          | Some l -> loop (f acc (Item.parse (Line.of_string_unsafe l)))
-        in
-        loop init
+    fold fn ~init ~f:(fun acc _ l ->
+        f acc (Item.parse l)
       )
 
   let save items fn =
--- lib/line_oriented.mli
+++ lib/line_oriented.mli
@@ -1,3 +1,45 @@
+(** Line-oriented file formats
+
+    This module features a few primitives that may help dealing with
+    file formats that are sequences of items that fit a single line,
+    aka line-oriented formats *)
+
+val fold :
+  string ->
+  init:'a ->
+  f:('a -> int -> Line.t -> 'a) ->
+  'a
+
+val fold_err :
+  string ->
+  init:'a ->
+  f:('a -> int -> Line.t -> ('a, 'b) result) ->
+  ('a, 'b) result
+
+(** A parser for lines
+
+    This module provides building blocks for a line parser, especially
+    a {!step} function, that given the state of the parser and a chunk
+    of bytes returns a list of lines and a new state. This API is
+    meant to be usable from various contexts, including async or lwt.
+*)
+module Parser : sig
+  (** Parser state *)
+  type state
+
+  (** The initial state to be fed to the {!step} function. *)
+  val initial_state : state
+
+  (** Number of lines seen so far. We have [line_number initial_state
+      = 0], and [line_number (fst (step _ (Some _))) > 0] *)
+  val line_number : state -> int
+
+  (** [step st i] parses an input. If [i = None], the caller indicates
+      that there is no more input; in that case the returned state is
+      terminal: all inputs read from this state will be ignored. *)
+  val step : state -> string option -> state * Line.t list
+end
+
 module type Item = sig
   type t
   val parse : Line.t -> t
@@ -8,7 +50,6 @@ module type S = sig
   type item
   val load : string -> item list
   val fold : string -> init:'a -> f:('a -> item -> 'a) -> 'a
-
   val save : item list -> string -> unit
 end
 
