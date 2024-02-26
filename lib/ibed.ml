66b28ffaa3c637971a61fb4309db27b401097914
tyrosine-rex <thomas.lahaie@icloud.com>
2024-02-26 14:22:09 +0100

    better printing unit test

--- lib/ibed.ml
+++ lib/ibed.ml
@@ -90,7 +90,7 @@ let to_file ?(header = true) f_path it_lst =
 (* for testing *)
 
 let printf_item it =
-  Printf.sprintf "%s --> %s\t (%s) --> (%s)(rd:%d;sc:%f)"
+  Printf.sprintf "    %s --> %s\t (%s) --> (%s)(rd:%d;sc:%f)"
     (GLoc.to_string it.bait)
     (GLoc.to_string it.other)
     (String.concat ";" it.bait_names)
@@ -98,43 +98,43 @@ let printf_item it =
     it.n_reads it.score
 
 let printf_it_lst it_lst =
-  String.concat "\n    " @@ List.map printf_item it_lst
+  String.concat "\n" @@ List.map printf_item it_lst
 
 let%expect_test "fromfile_sort_orderize_1" =
   Printf.printf "%s" (printf_it_lst @@ sort @@ from_file_exn "../data/test.ibed");
   [%expect {|
     1:100-200 --> 1:800-900	 (A1;A2;A3) --> (B1;B2)(rd:10;sc:1.234500)
-        2:2000-3000 --> 2:200-300	 (C1) --> (D1)(rd:100;sc:9.990000)
-        2:2300-9300 --> 2:1200-1300	 (C1) --> (D1)(rd:100;sc:9.990000)
-        5:1000-1500 --> 5:3000-3500	 (E1) --> (F1)(rd:99;sc:3.141590)
-        5:3000-3500 --> 5:60300-60800	 (K1) --> (L1;L2)(rd:729;sc:76.890000)
-        5:3200-3890 --> 5:600-800	 (I1) --> (J1;J2)(rd:999;sc:141.590000)
-        5:3200-3890 --> 5:70600-70800	 (I1) --> (Q1)(rd:876;sc:19.000000)
-        5:3200-4400 --> 5:7300-7800	 (M1;M2) --> (N1)(rd:29;sc:1.590000)
-        5:3900-4400 --> 5:11100-11750	 (O1) --> (P1)(rd:29;sc:1.590000)
-        X:2000-2500 --> X:12345-71000	 (G1;G2) --> (X1)(rd:3;sc:110.000000)
-        X:2000-2500 --> X:13579-81000	 (G1;G2) --> (Y1)(rd:4;sc:120.000000)
-        X:2000-2500 --> X:24680-91000	 (G1;G2) --> (Z1)(rd:5;sc:130.000000) |}]
+    2:2000-3000 --> 2:200-300	 (C1) --> (D1)(rd:100;sc:9.990000)
+    2:2300-9300 --> 2:1200-1300	 (C1) --> (D1)(rd:100;sc:9.990000)
+    5:1000-1500 --> 5:3000-3500	 (E1) --> (F1)(rd:99;sc:3.141590)
+    5:3000-3500 --> 5:60300-60800	 (K1) --> (L1;L2)(rd:729;sc:76.890000)
+    5:3200-3890 --> 5:600-800	 (I1) --> (J1;J2)(rd:999;sc:141.590000)
+    5:3200-3890 --> 5:70600-70800	 (I1) --> (Q1)(rd:876;sc:19.000000)
+    5:3200-4400 --> 5:7300-7800	 (M1;M2) --> (N1)(rd:29;sc:1.590000)
+    5:3900-4400 --> 5:11100-11750	 (O1) --> (P1)(rd:29;sc:1.590000)
+    X:2000-2500 --> X:12345-71000	 (G1;G2) --> (X1)(rd:3;sc:110.000000)
+    X:2000-2500 --> X:13579-81000	 (G1;G2) --> (Y1)(rd:4;sc:120.000000)
+    X:2000-2500 --> X:24680-91000	 (G1;G2) --> (Z1)(rd:5;sc:130.000000) |}]
 
 let%expect_test "fromfile_sort_orderize_2" =
   Printf.printf "%s" (printf_it_lst @@ sort ~desc:true @@ from_file_exn "../data/test.ibed");
   [%expect {|
     X:2000-2500 --> X:12345-71000	 (G1;G2) --> (X1)(rd:3;sc:110.000000)
-        X:2000-2500 --> X:13579-81000	 (G1;G2) --> (Y1)(rd:4;sc:120.000000)
-        X:2000-2500 --> X:24680-91000	 (G1;G2) --> (Z1)(rd:5;sc:130.000000)
-        5:3900-4400 --> 5:11100-11750	 (O1) --> (P1)(rd:29;sc:1.590000)
-        5:3200-4400 --> 5:7300-7800	 (M1;M2) --> (N1)(rd:29;sc:1.590000)
-        5:3200-3890 --> 5:600-800	 (I1) --> (J1;J2)(rd:999;sc:141.590000)
-        5:3200-3890 --> 5:70600-70800	 (I1) --> (Q1)(rd:876;sc:19.000000)
-        5:3000-3500 --> 5:60300-60800	 (K1) --> (L1;L2)(rd:729;sc:76.890000)
-        5:1000-1500 --> 5:3000-3500	 (E1) --> (F1)(rd:99;sc:3.141590)
-        2:2300-9300 --> 2:1200-1300	 (C1) --> (D1)(rd:100;sc:9.990000)
-        2:2000-3000 --> 2:200-300	 (C1) --> (D1)(rd:100;sc:9.990000)
-        1:100-200 --> 1:800-900	 (A1;A2;A3) --> (B1;B2)(rd:10;sc:1.234500) |}]
+    X:2000-2500 --> X:13579-81000	 (G1;G2) --> (Y1)(rd:4;sc:120.000000)
+    X:2000-2500 --> X:24680-91000	 (G1;G2) --> (Z1)(rd:5;sc:130.000000)
+    5:3900-4400 --> 5:11100-11750	 (O1) --> (P1)(rd:29;sc:1.590000)
+    5:3200-4400 --> 5:7300-7800	 (M1;M2) --> (N1)(rd:29;sc:1.590000)
+    5:3200-3890 --> 5:600-800	 (I1) --> (J1;J2)(rd:999;sc:141.590000)
+    5:3200-3890 --> 5:70600-70800	 (I1) --> (Q1)(rd:876;sc:19.000000)
+    5:3000-3500 --> 5:60300-60800	 (K1) --> (L1;L2)(rd:729;sc:76.890000)
+    5:1000-1500 --> 5:3000-3500	 (E1) --> (F1)(rd:99;sc:3.141590)
+    2:2300-9300 --> 2:1200-1300	 (C1) --> (D1)(rd:100;sc:9.990000)
+    2:2000-3000 --> 2:200-300	 (C1) --> (D1)(rd:100;sc:9.990000)
+    1:100-200 --> 1:800-900	 (A1;A2;A3) --> (B1;B2)(rd:10;sc:1.234500) |}]
 
 let%expect_test "group_by_bait" =
   let gb = group_by_bait @@ from_file_exn "../data/test.ibed" in
-  Printf.printf "%s" (String.concat "\n" @@ List.map (fun c -> Printf.sprintf "\nitem =>\n    %s\nitem_list =>\n    %s\n" (printf_item @@ fst c) (printf_it_lst @@ snd c)) gb);
+  Printf.printf "%s" (String.concat "\n" @@ List.map (fun c -> Printf.sprintf "\nitem =>\n%s\nitem_list =>\n%s\n" (printf_item @@ fst c) (printf_it_lst @@ snd c)) gb);
   [%expect {|
     item =>
         X:2000-2500 --> X:24680-91000	 (G1;G2) --> (Z1)(rd:5;sc:130.000000)
@@ -194,7 +194,7 @@ let%expect_test "group_by_bait" =
 
 let%expect_test "group_by_chr" =
   let gb = group_by_chr @@ from_file_exn "../data/test.ibed" in
-  Printf.printf "%s" (String.concat "\n" @@ List.map (fun (c, l) -> Printf.sprintf "\nitem =>\n    %s\nitem_list =>\n    %s\n" (printf_item c) (printf_it_lst l)) gb);
+  Printf.printf "%s" (String.concat "\n" @@ List.map (fun (c, l) -> Printf.sprintf "\nitem =>\n%s\nitem_list =>\n%s\n" (printf_item c) (printf_it_lst l)) gb);
   [%expect {|
     item =>
         X:2000-2500 --> X:24680-91000	 (G1;G2) --> (Z1)(rd:5;sc:130.000000)
